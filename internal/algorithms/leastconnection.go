d5848311108bcf81015649fe90c2b948983a1e9f
FelipeMCassiano <felipemcassiano@gmail.com>
2024-07-18 18:05:32 -0300

    feat: go back to only mutex operations

--- internal/algorithms/leastconnection.go
+++ internal/algorithms/leastconnection.go
@@ -2,13 +2,12 @@ package algorithms
 
 import (
 	"sync"
-	"sync/atomic"
 
 	"github.com/kauefraga/anubis/internal/models"
 )
 
 func LeastConnection() Algorithm {
-	var serversConnectionCount map[models.Servers]*int32
+	var serversConnectionCount map[models.Servers]int
 	var mu sync.Mutex
 	var once sync.Once
 
@@ -16,34 +15,32 @@ func LeastConnection() Algorithm {
 		// ensures that will be executed one time
 		once.Do(func() {
 			if serversConnectionCount == nil {
-				serversConnectionCount = make(map[models.Servers]*int32)
+				serversConnectionCount = make(map[models.Servers]int)
 
 				for _, s := range servers {
-					var count int32 = 0
-					serversConnectionCount[*s] = &count
+					serversConnectionCount[*s] = 0
 				}
 			}
 		})
 
 		mu.Lock()
 		defer mu.Unlock()
-    
+
 		var leastConnectionServer *models.Servers // least connection server
 
 		// magic number, minimum connections?
 		// ref: https://www.geeksforgeeks.org/load-balancing-algorithms/#21-least-connection-method-load-balancing-algorithms
-		var minConnections int32 = 1_000_000
+		var minConnections int = 1_000_000
 
 		for server, count := range serversConnectionCount {
-			currenctCount := atomic.LoadInt32(count)
-			if currenctCount < minConnections {
-				minConnections = currenctCount
+			if count < minConnections {
+				minConnections = count
 				leastConnectionServer = &server
 			}
 		}
 
 		if leastConnectionServer != nil {
-			atomic.AddInt32(serversConnectionCount[*leastConnectionServer], minConnections+1)
+			serversConnectionCount[*leastConnectionServer]++
 		}
 
 		return leastConnectionServer
